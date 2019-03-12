0365d7d6139bb21d741d0d3224ec3f4706bd37d4
AkramC <akramhostel3@gmail.com>
2019-03-12 11:32:45 +0530

    Spark Interview solution added

--- productdesc.py
+++ productdesc.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.types import *
-
+from pyspark.sql import Window
 spark = SparkSession.builder.appName("product desc").master("local").getOrCreate()
 schemaprod = StructType([
 StructField("Id",StringType(),True),
@@ -9,6 +9,8 @@ StructField("Productdesc",StringType(),True) ])
 product = spark.read.schema(schemaprod).csv("D://productdesc.csv")
 productexpl=product.withColumn("tshrt",F.split("Productdesc"," ")).select("Id","Productdesc",F.explode("tshrt").alias("words"))
 productfilter=productexpl.filter(F.col("words") == "Tshirt").drop("Productdesc")
-priducttshrt=productfilter.groupBy("Id").count().filter(F.col("count") == 2)
+win=Window.partitionBy("Id")
+priducttshrtt=productfilter.select("Id",F.count("Id").over(win).alias("Cnt")).filter(F.col("Cnt") == 2).distinct()
+#priducttshrt=productfilter.groupBy("Id").count().filter(F.col("count") == 2)
 
-priducttshrt.show()
+priducttshrtt.show()
